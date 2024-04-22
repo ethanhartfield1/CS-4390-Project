d968d8f3126fa35e4233ac47db00400e9a3ff4be
Zanawer <151594892+Zanawer@users.noreply.github.com>
2024-04-21 19:36:47 -0500

    Update Main.java
    The following changes add a loop so that after the client sends and receives a string from the server it can re-send more until the users chooses to terminate the conncection by typing "exit". Currently, it successfully exits and sends the first sting to the server with no issues. However, the server does not see the strings after the first responce so changes will need to be done there.

--- Main.java
+++ Main.java
@@ -6,32 +6,43 @@ class TCPClient {
     {
         String sentence;
         String modifiedSentence;
+        boolean clientConnection;
+        
         System.out.println("Client is running: " );
+        System.out.print("Enter a string to capitalize or type 'exit' to close the connection: ");
 
         Socket clientSocket = new Socket("127.0.0.1", 6789);
+        clientConnection = true; // allow for client to keep connection with server
 
-        BufferedReader inFromUser =
-                new BufferedReader(new InputStreamReader(System.in));
+      // create necessary objects for connection
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
-        BufferedReader inFromServer =
-                new BufferedReader(new
-                        InputStreamReader(clientSocket.getInputStream()));
+        //Loop requests till user types "exit"
+        while (clientConnection) {
 
-        DataOutputStream outToServer =
-                new DataOutputStream(clientSocket.getOutputStream());
+            sentence = inFromUser.readLine();
 
+            if (sentence.equalsIgnoreCase("exit")) {
+                clientSocket.close();
+                clientConnection = false;
+            }
+            else {
+                System.out.println("sent...");
 
+                outToServer.flush(); // was running into buffer issues
+                outToServer.writeBytes(sentence + '\n');
+                modifiedSentence = inFromServer.readLine();
 
-        sentence = inFromUser.readLine();
+                System.out.println("FROM SERVER: " + modifiedSentence);
+                System.out.print("Enter next string or type 'exit' to close connection: ");
+            }
 
-
-        outToServer.writeBytes(sentence + '\n');
-
-        modifiedSentence = inFromServer.readLine();
-
-        System.out.println("FROM SERVER: " + modifiedSentence);
-
-        clientSocket.close();
+            System.out.println("Connection Terminated");
+        }
 
     }
 }
+
+
