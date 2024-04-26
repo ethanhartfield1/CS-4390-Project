9396d8a94105648f640b2410f613e9568d14f6d9
Ethan Hartfield <64440841+ethanhartfield1@users.noreply.github.com>
2024-04-25 19:12:08 -0500

    Update Server4390.java
    Basic math added. Needs to be cleaned up a lot.

--- Server4390.java
+++ Server4390.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 class TCPServer {
 
@@ -21,34 +23,66 @@ class TCPServer {
     public static class clientThread extends Thread {
         String clientSentence;
         String capitalizedSentence;
-
+        String userName;
+        String timeJoined;
         private Socket connectionSocket;
-
         public clientThread(Socket connectionSocket) {this.connectionSocket = connectionSocket;}
-        
+
+
         //Loop for the thread connection.
         public void run() {
+
             try {
                 // Create communication streams (same as sample codes)
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                 DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
+                userName = inFromClient.readLine();
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                LocalDateTime timeJoined= LocalDateTime.now();
+                long startTime = System.currentTimeMillis();
+                System.out.println(dtf.format(timeJoined));
+                System.out.println("Received username from client: " + userName);
+                outToClient.write(1);
                 // Process messages until client disconnects (by typing exit)
+                String op = "";
+                double number = 0;
                 while (true) {
-                    clientSentence = inFromClient.readLine();
-                    if (clientSentence == null) { // when user types exit the server gets an empty response (will happen as client disconnects from server)
+
+                    String string = inFromClient.readLine();
+                    if (string.equals("+")||string.equals("-")||string.equals("*")||string.equals("/")){
+                        op = string;
+                    } else if (op.equals("")) number = Double.parseDouble(string);
+                    else{
+                        if (op.equals("+")) number += Double.parseDouble(string);
+                        else if (op.equals("-")) number -= Double.parseDouble(string);
+                        else if (op.equals("*")) number *= Double.parseDouble(string);
+                        else if (op.equals("/")) number /= Double.parseDouble(string);
+                        op = "";
+                    }
+
+                    if (string == null) { // when user types exit the server gets an empty response (will happen as client disconnects from server)
                         break;
                     }
-                    System.out.println("Server received message! (" + clientSentence + ")");
-                    capitalizedSentence = clientSentence.toUpperCase() + '\n';// still capitalizes but goal is to calculate strings)
-                    outToClient.writeBytes(capitalizedSentence);
+                    System.out.println("Server received a message from "+ userName + "! (" + number + op + ")");
+
+                    outToClient.writeBytes(String.valueOf(number) + op + "\n");
                 }
                 // Close connection with individual client
                 connectionSocket.close();
+                long endTime = System.currentTimeMillis();
+                String timeElapsed = formatDuration(endTime-startTime);
+                System.out.println(userName + " disconnected with " + timeElapsed + " time elapsed.");
             } catch (IOException e) {
                 // Handle any exceptions during communication
                 e.printStackTrace();
             }
+        }
+        private String formatDuration(long millis){
+            long seconds = millis / 1000;
+            long minutes = seconds / 60;
+            seconds = seconds%60;
+                    return String.format("%d minute(s) %d second(s)", minutes, seconds);
         }
     }
 }
