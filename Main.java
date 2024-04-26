9d15581c7fcc9427928f79e1971cb2b3a3553780
Ethan Hartfield <64440841+ethanhartfield1@users.noreply.github.com>
2024-04-25 19:12:48 -0500

    Update Main.java
    Not much changed here. might want to make variables consistent

--- Main.java
+++ Main.java
@@ -1,30 +1,42 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+
 class TCPClient {
 
     public static void main(String argv[]) throws Exception
     {
-        String sentence;
+        String string;
         String modifiedSentence;
+        String userName;
         boolean clientConnection;
-        
-        System.out.println("Client is running: " );
-        System.out.print("Enter a string to capitalize or type 'exit' to close the connection: ");
+
+        System.out.println("Client is running");
+        System.out.print("Enter a name to be used for the server:");
 
         Socket clientSocket = new Socket("127.0.0.1", 6789);
         clientConnection = true; // allow for client to keep connection with server
 
-      // create necessary objects for connection
+        // create necessary objects for connection
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        userName = inFromUser.readLine();
 
-        //Loop requests till user types "exit"
+        outToServer.writeBytes(userName + '\n');
+        outToServer.flush();
+        int confirmation = inFromServer.read();
+        while (confirmation != 1) {
+            ;//Loop until gets a true
+        }
+        System.out.println("Acknowledgement from server received!");
+
+        System.out.print("Enter any integer or double with four basic math operations (+, -, *, /) or type 'exit' to close the connection: ");
+
         while (clientConnection) {
+            string = inFromUser.readLine();
 
-            sentence = inFromUser.readLine();
-
-            if (sentence.equalsIgnoreCase("exit")) {
+            if (string.equalsIgnoreCase("exit")) {
                 clientSocket.close();
                 clientConnection = false;
             }
@@ -32,17 +44,13 @@ class TCPClient {
                 System.out.println("sent...");
 
                 outToServer.flush(); // was running into buffer issues
-                outToServer.writeBytes(sentence + '\n');
-                modifiedSentence = inFromServer.readLine();
-
-                System.out.println("FROM SERVER: " + modifiedSentence);
+                outToServer.writeBytes(string + '\n');
+                String currentNumber = inFromServer.readLine();
+                System.out.println("FROM SERVER: " + currentNumber);
                 System.out.print("Enter next string or type 'exit' to close connection: ");
             }
-
-            System.out.println("Connection Terminated");
         }
 
+        System.out.println("Connection Terminated");
     }
 }
-
-
